e2ac965a8a8ad08e89191b7f96200907f979ef95
ShenQi1996 <qisam1989@gmail.com>
2021-04-27 16:38:04 -0400

    up to Styling

--- widgets/frontend/clock.jsx
+++ widgets/frontend/clock.jsx
@@ -1,20 +1,36 @@
 import React from "react"
 
 class Clock extends React.Component {
-    constructor(){
-        // let time = new Date()
+    constructor(props){
+        super(props)
         this.state = {
-        //    hour: time.getHours(),
-        //    minute: time.getMinutes(),
-        //    second: time.getSeconds(),
-        
+            time: new Date().toLocaleString()
         }
+        this.tick = this.tick.bind(this)
+        this.interval = setInterval(() => this.tick(), 1000)
     }
 
 
+    tick(){
+        this.setState({
+            time: new Date().toLocaleString()
+        })
+    }
+
+    componentDidMount(){
+        console.log("we are inside did Mount!!!!")
+        this.interval;
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.interval)
+    }
+
     render() {
         return(
-            <h1>111111</h1>
+            <h1>
+                {this.state.time}
+            </h1>
         )
     }
 }
